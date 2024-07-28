a658353732ac050d0826e7f1d69789e18de2ec8d
ryuselmi <yuselmirodriguez24@gmail.com>
2024-07-28 12:50:26 -0700

    Add project files.

---
+++ CSC_295_HW4/LInkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSC295HW4.LinkedList
+{
+    public class LinkedList
+    {
+        public Node? First { get; set; }
+
+        public void InsertFirst(int data)
+        {
+            Node newNode = new Node();
+
+            newNode.Data = data;
+
+            newNode.Next = First;
+
+            First = newNode;
+        }
+
+        public Node DeleteFirst(int data)
+        {
+            if (First == null)
+            {
+                throw new InvalidOperationException("Cannot delete from an empty list.");
+            }
+            Node temp = First;
+            First = First.Next;
+            return temp;
+        }
+
+        public void DisplayList()
+        {
+            Console.WriteLine("Iterating thru list...");
+            Node? current = First;
+            while (current != null)
+            {
+                current.DisplayNode();
+                current = current.Next;
+            }
+        }
+
+        public void InsertLast(int data)
+        {
+            Node newNode = new Node();
+            newNode.Data = data;
+
+            if (First == null)
+            {
+                First = newNode;
+            }
+            else
+            {
+                Node current = First;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+    }
+}
---
+++ CSC_295_HW4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Linked List
+using CSC295HW4.LinkedList;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList linkedList = new LinkedList();
+        linkedList.InsertFirst(1);
+        linkedList.InsertFirst(2);
+        linkedList.InsertFirst(3);
+        linkedList.InsertFirst(4);
+
+        linkedList.DeleteFirst(1);
+        linkedList.DeleteFirst(2);
+
+        linkedList.InsertLast(926);
+        linkedList.InsertLast(348);
+
+        linkedList.DisplayList();
+    }
+}
